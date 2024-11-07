78dae1a381462c8338e4f8f1f238d0c56ce5e656
WilliamCely <william.cely05@uptc.edu.co>
2024-11-07 09:54:01 -0500

    index

--- src/main/java/edu/uptc/swii/libraryapp/controller/ListadoController.java
+++ src/main/java/edu/uptc/swii/libraryapp/controller/ListadoController.java
@@ -1,9 +1,15 @@
 package edu.uptc.swii.libraryapp.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
 
 @Controller
 public class ListadoController {
    
-    
+ @RequestMapping("/")
+public String listarLibros(){
+    return "listado";
+}
 }
